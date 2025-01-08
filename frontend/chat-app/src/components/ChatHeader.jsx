0204304409ee695d77edb308828819fe65d28b4b
Nikhil <nikhilnitin3@gmail.com>
2025-01-08 21:50:21 +0530

    Header changed

--- frontend/chat-app/src/components/ChatHeader.jsx
+++ frontend/chat-app/src/components/ChatHeader.jsx
@@ -16,7 +16,7 @@ const ChatHeader = () => {
           <div className="avatar">
             <div className="size-10 rounded-full relative">
               <img
-                src={selectedUser.profilePic || "/avatar.png"}
+                src={selectedUser.profile|| "/avatar.png"}
                 alt={selectedUser.fullName}
               />
             </div>
